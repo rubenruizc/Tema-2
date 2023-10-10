aa3580441f9883ae30681bed1e3da0bbec2b4be9
rubenruizc <ruben.ruiz@iesnervion.es>
2023-10-10 13:45:00 +0200

    Ejercicio 4 T2 2ºParte

--- EjerciciosT2/src/ejerciciost2_2/Ejercicio4T2_2.java
+++ EjerciciosT2/src/ejerciciost2_2/Ejercicio4T2_2.java
@@ -5,14 +5,38 @@ import java.util.Scanner;
 public class Ejercicio4T2_2 {
 
 	public static void main(String[] args) {
-		String tirada1, tirada2;
-		int valorTirada1 = 0, valorTirada2 = 0;
+
+		// Variable donde guardaremos la primera tirada del usuario
+		String tirada1;
+		
+		// Variable donde guardaremos la segunda tirada del usuario
+		String tirada2;
+		
+		// Variable donde guardaremos el valor de la 1º tirada en números
+		int valorTirada1 = 0;
+		
+		// Variable donde guardaremos el valor de la 2º tirada en números
+		int valorTirada2 = 0;
+		
+		// Variable donde guardaremos el valor total de las dos tiradas
 		int valorTotal;
-		Scanner sc = new Scanner (System.in);
+		
+		// Scanner para leer del teclado
+		Scanner sc = new Scanner(System.in);
+		
+		// Le preguntamos por la primera tirada al usuario (Con letras)
 		System.out.println("¿Cuánto ha sacado en la primera tirada?");
+		
+		// Leemos la línea
 		tirada1 = sc.nextLine();
+		
+		// Le preguntamos por la segunda tirada al usuario (Con letras)
 		System.out.println("¿Cuánto ha sacado en la seguunda tirada?");
+		
+		// Leemos la línea
 		tirada2 = sc.nextLine();
+		
+		// Le damos valores a los números introducidos con letras (1ºtirada)
 		switch (tirada1) {
 		case "UNO" -> valorTirada1 = 1;
 		case "DOS" -> valorTirada1 = 2;
@@ -20,24 +44,32 @@ public class Ejercicio4T2_2 {
 		case "CUATRO" -> valorTirada1 = 4;
 		case "CINCO" -> valorTirada1 = 5;
 		case "SEIS" -> valorTirada1 = 6;
-		default ->
-		System.out.println("NÚMERO MAL INTRODUCIDO");
 		}
+
+		// Le damos valores a los números introducidos con letras (2ºtirada)
 		switch (tirada2) {
-		case "UNO" -> valorTirada2 = 1;
+		case "UNO" ->{ valorTirada2 = 1;
+		System.out.println();
+		}
 		case "DOS" -> valorTirada2 = 2;
 		case "TRES" -> valorTirada2 = 3;
 		case "CUATRO" -> valorTirada2 = 4;
 		case "CINCO" -> valorTirada2 = 5;
-		case "SEIS" -> valorTirada2 = 6 ;
-		default ->
-		System.out.println("NÚMERO MAL INTRODUCIDO");
+		case "SEIS" -> valorTirada2 = 6;
 		}
 		
+		// Operación para calcular el total de las tiradas
 		valorTotal = valorTirada1 + valorTirada2;
-		System.out.println("Su tirada da un total de: " + valorTotal);
-		
-		sc.close();
-	}
 
-}
+		// Comprobamos si están bien elegidos los números
+		if (valorTirada2 == 0 || valorTirada1 == 0) {
+			System.out.println("TIRADA NO VÁLIDA");
+		} else
+			System.out.println("Su tirada final es: " + valorTotal);
+
+		// Cerramos el Scanner
+		sc.close();
+		
+	}// Cierre main
+
+}// Cierre clase
