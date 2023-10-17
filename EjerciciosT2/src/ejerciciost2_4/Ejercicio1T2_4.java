e1e81d956e3b14cfb935bd3a3f19c952f6d6dae7
rubenruizc <ruben.ruiz@iesnervion.es>
2023-10-17 14:41:49 +0200

    Ejercicios 1 y 2 T2 4º Parte

---
+++ EjerciciosT2/src/ejerciciost2_4/Ejercicio1T2_4.java
@@ -0,0 +1,18 @@
+package ejerciciost2_4;
+
+public class Ejercicio1T2_4 {
+
+	public static void main(String[] args) {
+		int num = 1;
+		do {
+			System.out.println(num);
+			num++;
+			
+		} while (num <=20) ;
+		
+			
+		}
+
+	}
+
+
---
+++ EjerciciosT2/src/ejerciciost2_4/Ejercicio2T2_4.java
@@ -0,0 +1,16 @@
+package ejerciciost2_4;
+
+public class Ejercicio2T2_4 {
+
+	public static void main(String[] args) {
+		int num = 1;
+		do {
+			if (num % 2 == 0) 
+				System.out.println(num);
+				num++;
+			 
+		} while (num <=200);
+
+	}
+
+}
